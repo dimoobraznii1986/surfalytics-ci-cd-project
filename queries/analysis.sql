be13acf690fdb097ae4653198ca7821540fa9b14
Dmitry Anoshin <98490741+dimoobraznii1986@users.noreply.github.com>
2024-12-20 08:56:12 -0800

    Update SQL

--- queries/analysis.sql
+++ queries/analysis.sql
@@ -8,7 +8,7 @@ SELECT
     MAX(total_cases) as max_total_cases,
     MAX(total_deaths) as max_total_deaths,
     MAX(total_cases_per_million) as max_cases_per_million
-FROM covid_data WHERE continent IS NULL --NOT NULL
+FROM covid_data WHERE continent = 'Canada'--IS NULL --NOT NULL
 GROUP BY location ORDER BY max_total_cases DESC LIMIT 20;
 
 -- Output results to CSV
